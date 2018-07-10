714838d41ee732817e69bed0a9cebfa4b9cf5fca
Kevin Rao <krao@Kevins-MacBook-Pro-7.local>
2018-07-10 17:30:30 -0400

    tried putting into plots

--- regresso.R
+++ regresso.R
@@ -10,15 +10,24 @@ for(i in 12:534){
     temp <- filter(temp, complete.cases(temp))
     pairCors[ct] <- cor(temp[1], temp[2])
     if(pairCors[ct] > .25){
-      print(paste(colnames(temp)[1], 'and', colnames(temp)[2], 'have correlation', pairCors[ct], sep = ' '))
+      print(paste(colnames(temp)[1], 'and', colnames(temp)[2], 'are columns (', i, ',', j, ') and have correlation', pairCors[ct], sep = ' '))
     }
     ct <- ct + 1
   }
 }
 
-temp <- pairCors[order(-pairCors)]
+orderedCors <- pairCors[order(-pairCors)]
 
-head(temp)
+head(orderedCors)
+
+
+#put the highest correlation variables in a regression
+
+ggplot(data = md) +
+  geom_point(mapping = aes(x = mzid_445.227362_3.4312, y = beer))
+
+ggplot(data = md) +
+  geom_point(mapping = aes(x = mzid_327.217613_4.0425, y = mzid_329.232478_3.9113))
 
 skrrrahh(16)
 
